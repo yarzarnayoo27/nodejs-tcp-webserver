289134e9884d335ba4b5b5da7020a6a78bd64264
Yar Zar Nay Oo <yarzarnayoo27@gmail.com>
2025-11-13 22:09:15 +0630

    Finalize a Node.js webserver with TCP

--- nodejsTCPWebserver.js
+++ nodejsTCPWebserver.js
@@ -2,6 +2,18 @@ const net = require("node:net");
 
 let server = new net.Server();
 
+function respond(socket, statusCode, statusMessage, body) {
+  let httpResponse =
+    `HTTP/1.1 ${statusCode} ${statusMessage}\r\n` +
+    "Content-Type: text/html\r\n" +
+    "Connection: close\r\n" +
+    `Content-Length: ${body.length}\r\n` +
+    "\r\n" +
+    body;
+  socket.write(httpResponse);
+  socket.end();
+}
+
 server.on("connection", (socket) => {
   console.log(`remoteAddress:   ${socket.remoteAddress}`);
   console.log(`remotePort:      ${socket.remotePort}`);
@@ -9,13 +21,12 @@ server.on("connection", (socket) => {
   console.log(`localAddress:    ${socket.localAddress}`);
   console.log(`localPort:       ${socket.localPort}`);
 
-  socket.on("data", data => {
+  socket.on("data", (data) => {
     let httpRequest = data.toString();
     let lines = httpRequest.split("\r\n");
     let statusLine = lines[0];
     let requestUrl = statusLine.split(" ")[1];
-    console.log(requestUrl);
-    socket.end();
+    respond(socket, 200, "OK", `<h1>You requested: ${requestUrl}</h1>`);
   });
 });
 
